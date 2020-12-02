e2d2906c8077ead3f03a0ae4e9718a055fa059c7
tarpaha <ppopchikovsky@gmail.com>
2020-12-02 14:25:54 +0300

    parser tests converted to test cases

--- day_2020_12_02.tests/ParserTests.cs
+++ day_2020_12_02.tests/ParserTests.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -6,20 +5,12 @@ namespace day_2020_12_02.tests
 {
     public class ParserTests
     {
-        private static IEnumerable<TestCaseData> ParserTestCases
+        [TestCase("1-3 a: abcde", 1, 3, 'a', "abcde")]
+        [TestCase("1-3 b: cdefg", 1, 3, 'b', "cdefg")]
+        [TestCase("2-9 c: ccccccccc", 2, 9, 'c', "ccccccccc")]
+        public void Parse_Works_Correctly(string data, int min, int max, char ch, string password)
         {
-            get
-            {
-                yield return new TestCaseData("1-3 a: abcde", new Record(1, 3, 'a', "abcde"));
-                yield return new TestCaseData("1-3 b: cdefg", new Record(1, 3, 'b', "cdefg"));
-                yield return new TestCaseData("2-9 c: ccccccccc", new Record(2, 9, 'c', "ccccccccc"));
-            }
-        }
-        
-        [TestCaseSource(nameof(ParserTestCases))]
-        public void Parse_Works_Correctly(string s, Record result)
-        {
-            Parser.Parse(s).Should().BeEquivalentTo(result);
+            Parser.Parse(data).Should().BeEquivalentTo(new Record(min, max, ch, password));
         }
     }
 }
