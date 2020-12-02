b67e505ff8cfc44a4633a83bdbd2a6fae46cf508
tarpaha <ppopchikovsky@gmail.com>
2020-12-02 14:26:12 +0300

    solver part1 with tests

--- day-2020-12-02/Solver.cs
+++ day-2020-12-02/Solver.cs
@@ -1,6 +1,26 @@
-﻿namespace day_2020_12_02
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace day_2020_12_02
 {
-    public class Solver
+    public static class Solver
     {
+        public static int Part1(IEnumerable<Record> records)
+        {
+            return records
+                .AsParallel()
+                .Count(PasswordIsValid);
+        }
+
+        public static bool PasswordIsValid(Record record)
+        {
+            var count = record
+                .Password
+                .GroupBy(ch => ch)
+                .FirstOrDefault(group => group.Key == record.Char)
+                ?.Count();
+            
+            return count.HasValue && record.Min <= count && count <= record.Max;
+        }
     }
 }
---
+++ day_2020_12_02.tests/SolverTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace day_2020_12_02.tests
+{
+    public class SolverTests
+    {
+        [TestCase(1, 3, 'a', "abcde", true)]
+        [TestCase(1, 3, 'b', "cdefg", false)]
+        [TestCase(2, 9, 'c', "ccccccccc", true)]
+        public void PasswordIsValid_Works_Correctly(int min, int max, char ch, string password, bool result)
+        {
+            Assert.That(Solver.PasswordIsValid(new Record(min, max, ch, password)), Is.EqualTo(result));
+        }
+    }
+}
