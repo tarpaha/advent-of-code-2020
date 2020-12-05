418f63702c57a6d8ad176dc0ce86f9d199f028d2
tarpaha <ppopchikovsky@gmail.com>
2020-12-05 21:22:34 +0300

    initial structure

---
+++ day-2020-12-05.tests/SolverTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace day_2020_12_05.tests
+{
+    public class SolverTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
