17c7b6c02036763796b137be04583f0df07671da
tarpaha <ppopchikovsky@gmail.com>
2020-12-06 13:24:54 +0300

    initial structure

---
+++ day-2020-12-06.app/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using utils;
+
+namespace day_2020_12_06.app
+{
+    public class Solution : ISolution
+    {
+        public static void Main()
+        {
+            var solution = new Solution();
+            Console.WriteLine($"Part1: {solution.SolvePart1()}");
+            Console.WriteLine($"Part2: {solution.SolvePart2()}");
+        }
+
+        public object SolvePart1()
+        {
+            return default;
+        }
+
+        public object SolvePart2()
+        {
+            return default;
+        }
+    }
+}
---
+++ day-2020-12-06/Solver.cs
@@ -0,0 +1,6 @@
+﻿namespace day_2020_12_06
+{
+    public static class Solver
+    {
+    }
+}
