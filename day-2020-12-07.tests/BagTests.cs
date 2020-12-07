c469bec6a83845ea46563b82e93d2131f42b7f88
tarpaha <ppopchikovsky@gmail.com>
2020-12-07 13:58:02 +0300

    bag with tests

---
+++ day-2020-12-07.tests/BagTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace day_2020_12_07.tests
+{
+    public class BagTests
+    {
+        [Test]
+        public void AddInnerBag_Works_Correctly()
+        {
+            var a = new Bag("A");
+            var b = new Bag("B");
+            var c = new Bag("C");
+            
+            a.AddInnerBag(b, 1);
+            a.AddInnerBag(c, 2);
+            
+            Assert.That(a.OuterBags.Count(), Is.Zero);
+            Assert.That(a.InnerBags, Is.EquivalentTo(new [] { (b, 1), (c, 2) }));
+            
+            Assert.That(b.InnerBags.Count(), Is.Zero);
+            Assert.That(b.OuterBags, Is.EquivalentTo(new [] { a }));
+
+            Assert.That(c.InnerBags.Count(), Is.Zero);
+            Assert.That(c.OuterBags, Is.EquivalentTo(new [] { a }));
+        }
+    }
+}
---
+++ day-2020-12-07/Bag.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace day_2020_12_07
+{
+    public class Bag
+    {
+        public string Name { get; }
+        public IEnumerable<Bag> OuterBags => _outerBags;
+        public IEnumerable<(Bag, int)> InnerBags => _innerBags;
+
+        public Bag(string name)
+        {
+            Name = name;
+        }
+
+        public void AddInnerBag(Bag bag, in int count)
+        {
+            _innerBags.Add((bag, count));
+            bag._outerBags.Add(this);
+        }
+        
+        private readonly List<Bag> _outerBags = new List<Bag>();
+        private readonly List<(Bag, int)> _innerBags = new List<(Bag, int)>();
+    }
+}
