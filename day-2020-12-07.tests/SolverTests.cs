02e8a4225a8b79b68eb54b66f6800c30f2688f14
tarpaha <ppopchikovsky@gmail.com>
2020-12-07 17:03:11 +0300

    test case instead of test

--- day-2020-12-07.tests/SolverTests.cs
+++ day-2020-12-07.tests/SolverTests.cs
@@ -4,11 +4,8 @@ namespace day_2020_12_07.tests
 {
     public class SolverTests
     {
-        [Test]
-        public void Part1()
-        {
-            const string data = @"
-light red bags contain 1 bright white bag, 2 muted yellow bags.
+        [TestCase(
+@"light red bags contain 1 bright white bag, 2 muted yellow bags.
 dark orange bags contain 3 bright white bags, 4 muted yellow bags.
 bright white bags contain 1 shiny gold bag.
 muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
@@ -16,8 +13,10 @@ shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
 dark olive bags contain 3 faded blue bags, 4 dotted black bags.
 vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 faded blue bags contain no other bags.
-dotted black bags contain no other bags.";
-            Assert.That(Solver.Part1(Parser.Parse(data)), Is.EqualTo(4));
+dotted black bags contain no other bags.", 4)]
+        public void Part1(string data, int result)
+        {
+            Assert.That(Solver.Part1(Parser.Parse(data)), Is.EqualTo(result));
         }
         
         [Test]
