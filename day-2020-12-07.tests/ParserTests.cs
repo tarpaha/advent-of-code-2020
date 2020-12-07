7def9ddd66f2f2ad9dc7dfa517afba9632a3660a
tarpaha <ppopchikovsky@gmail.com>
2020-12-07 13:12:40 +0300

    parse line and tests

---
+++ day-2020-12-07.tests/ParserTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace day_2020_12_07.tests
+{
+    public class ParserTests
+    {
+        private static IEnumerable<TestCaseData> ParseLineTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+                    "light red", new [] { ("bright white", 1), ("muted yellow", 2) });
+                yield return new TestCaseData(
+                    "bright white bags contain 1 shiny gold bag.",
+                    "bright white", new [] { ("shiny gold", 1) });
+                yield return new TestCaseData(
+                    "faded blue bags contain no other bags.",
+                    "faded blue", Enumerable.Empty<(string, int)>());
+            }
+        }
+        
+        [TestCaseSource(nameof(ParseLineTestCases))]
+        public void ParseLine_Works_Correctly(string line, string resultName, IEnumerable<(string, int)> resultBags)
+        {
+            var (name, bags) = Parser.ParseLine(line);
+            Assert.That(name, Is.EqualTo(resultName));
+            Assert.That(bags, Is.EquivalentTo(resultBags));
+        }
+    }
+}
---
+++ day-2020-12-07/Parser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day_2020_12_07
+{
+    public static class Parser
+    {
+        public static (string name, IEnumerable<(string, int)> bags) ParseLine(string line)
+        {
+            var words = line.Split(new[] {' ', ',', '.'}, StringSplitOptions.RemoveEmptyEntries);
+            var name = $"{words[0]} {words[1]}";
+
+            var bags = new List<(string, int)>();
+            
+            if (words.Length > 7)
+            {
+                for (var p = 4; p < words.Length; p += 4)
+                {
+                    bags.Add(($"{words[p+1]} {words[p+2]}", int.Parse(words[p])));
+                }
+            }
+            
+            return (name, bags);
+        }
+    }
+}
