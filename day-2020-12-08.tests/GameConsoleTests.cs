2c2561a40e5296ab775052c6a04e34d82b0718fa
tarpaha <ppopchikovsky@gmail.com>
2020-12-08 13:00:41 +0300

    game console with tests

---
+++ day-2020-12-08.tests/GameConsoleTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace day_2020_12_08.tests
+{
+    public class GameConsoleTests
+    {
+        [TestCase(@"
+nop +0
+acc +2
+nop +0
+acc -1", 1)]
+        [TestCase(@"
+jmp +2
+jmp +3
+jmp -1
+jmp -99
+acc +3
+", 3)]
+        public void Program_Executes_Correctly(string lines, int result)
+        {
+            var gameConsole = new GameConsole();
+            gameConsole.LoadProgram(Parser.Parse(lines));
+            gameConsole.Start();
+            Assert.That(gameConsole.Accumulator, Is.EqualTo(result));
+        }
+    }
+}
---
+++ day-2020-12-08/GameConsole.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace day_2020_12_08
+{
+    public class GameConsole
+    {
+        public int Accumulator => _accumulator;
+        
+        public void LoadProgram(IEnumerable<Instruction> instructions)
+        {
+            _program = instructions.ToArray();
+        }
+
+        public void Start()
+        {
+            _instructionPointer = 0;
+            _accumulator = 0;
+
+            _infiniteLoopFlag = false;
+            _visitedPointers.Clear();
+
+            while (_instructionPointer < _program.Length)
+            {
+                if (_visitedPointers.Contains(_instructionPointer))
+                {
+                    _infiniteLoopFlag = true;
+                    break;
+                }
+
+                var instruction = _program[_instructionPointer];
+                switch (instruction.Operation)
+                {
+                    case Operation.nop:
+                        break;
+                    case Operation.acc:
+                        _accumulator += instruction.Argument;
+                        break;
+                    case Operation.jmp:
+                        _instructionPointer += instruction.Argument - 1;
+                        break;
+                    default:
+                        throw new Exception();
+                }
+
+                _instructionPointer += 1;
+            }
+        }
+        
+        private Instruction[] _program;
+        
+        private int _instructionPointer;
+        private int _accumulator;
+
+        private bool _infiniteLoopFlag;
+        private readonly HashSet<int> _visitedPointers = new HashSet<int>();
+    }
+}
