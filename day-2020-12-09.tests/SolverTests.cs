3eb1c4022080517945573983c7edb93b80ac8374
tarpaha <ppopchikovsky@gmail.com>
2020-12-09 13:19:53 +0300

    proper test name

--- day-2020-12-09.tests/SolverTests.cs
+++ day-2020-12-09.tests/SolverTests.cs
@@ -12,7 +12,7 @@ namespace day_2020_12_09.tests
         [TestCase(new long[] {20,21,2,15,3,7,25,5,24,23,14,18,16,11,9,13,6,1,10,8,19,12,22,4,17,45}, 1, 25, 65, false)]
         [TestCase(new long[] {20,21,2,15,3,7,25,5,24,23,14,18,16,11,9,13,6,1,10,8,19,12,22,4,17,45}, 1, 25, 64, true)]
         [TestCase(new long[] {20,21,2,15,3,7,25,5,24,23,14,18,16,11,9,13,6,1,10,8,19,12,22,4,17,45}, 1, 25, 66, true)]
-        public void HaveTwoNumbersWithSum(long[] numbers, int offset, int len, long sum, bool result)
+        public void HaveTwoNumbersWithSum_Works_Correctly(long[] numbers, int offset, int len, long sum, bool result)
         {
             Assert.That(Solver.HaveTwoNumbersWithSum(numbers, offset, len, sum), Is.EqualTo(result));
         }
