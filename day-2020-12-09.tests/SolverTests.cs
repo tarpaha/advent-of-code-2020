d545603106053144e1921234b9ef909d082f5a36
tarpaha <ppopchikovsky@gmail.com>
2020-12-09 13:02:47 +0300

    sum function with tests

--- day-2020-12-09.tests/SolverTests.cs
+++ day-2020-12-09.tests/SolverTests.cs
@@ -4,5 +4,17 @@ namespace day_2020_12_09.tests
 {
     public class SolverTests
     {
+        [TestCase(new long[] {1, 2, 3}, 0, 3, 1, false)]
+        [TestCase(new long[] {1, 2, 3}, 0, 3, 2, false)]
+        [TestCase(new long[] {1, 2, 3}, 0, 3, 3, true)]
+        [TestCase(new long[] {1, 2, 3}, 0, 3, 4, true)]
+        [TestCase(new long[] {1, 2, 3}, 0, 3, 5, true)]
+        [TestCase(new long[] {1, 2, 3}, 0, 2, 3, true)]
+        [TestCase(new long[] {1, 2, 3}, 1, 2, 3, false)]
+        [TestCase(new long[] {1, 2, 3}, 1, 2, 5, true)]
+        public void HaveTwoNumbersWithSum(long[] numbers, int offset, int len, long sum, bool result)
+        {
+            Assert.That(Solver.HaveTwoNumbersWithSum(numbers, offset, len, sum), Is.EqualTo(result));
+        }
     }
 }
--- day-2020-12-09/Solver.cs
+++ day-2020-12-09/Solver.cs
@@ -1,6 +1,20 @@
-﻿namespace day_2020_12_09
+﻿using System.Collections.Generic;
+
+namespace day_2020_12_09
 {
-    public class Solver
+    public static class Solver
     {
+        public static bool HaveTwoNumbersWithSum(long[] numbers, int offset, int len, long sum)
+        {
+            var residuals = new HashSet<long>();
+            for (var i = 0; i < len; i++)
+            {
+                var number = numbers[offset + i];
+                if (residuals.Contains(number))
+                    return true;
+                residuals.Add(sum - number);
+            }
+            return false;
+        }
     }
 }
