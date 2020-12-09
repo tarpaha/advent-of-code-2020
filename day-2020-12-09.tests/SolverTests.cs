c28b8429bca40a64d921eb851a70d44d1d1b66c5
tarpaha <ppopchikovsky@gmail.com>
2020-12-09 13:19:11 +0300

    complex sum tests

--- day-2020-12-09.tests/SolverTests.cs
+++ day-2020-12-09.tests/SolverTests.cs
@@ -4,14 +4,14 @@ namespace day_2020_12_09.tests
 {
     public class SolverTests
     {
-        [TestCase(new long[] {1, 2, 3}, 0, 3, 1, false)]
-        [TestCase(new long[] {1, 2, 3}, 0, 3, 2, false)]
-        [TestCase(new long[] {1, 2, 3}, 0, 3, 3, true)]
-        [TestCase(new long[] {1, 2, 3}, 0, 3, 4, true)]
-        [TestCase(new long[] {1, 2, 3}, 0, 3, 5, true)]
-        [TestCase(new long[] {1, 2, 3}, 0, 2, 3, true)]
-        [TestCase(new long[] {1, 2, 3}, 1, 2, 3, false)]
-        [TestCase(new long[] {1, 2, 3}, 1, 2, 5, true)]
+        [TestCase(new long[] {15,21,2,20,3,7,25,5,24,23,14,18,16,11,9,13,6,1,10,8,19,12,22,4,17}, 0, 25, 26, true)]
+        [TestCase(new long[] {15,21,2,20,3,7,25,5,24,23,14,18,16,11,9,13,6,1,10,8,19,12,22,4,17}, 0, 25, 49, true)]
+        [TestCase(new long[] {15,21,2,20,3,7,25,5,24,23,14,18,16,11,9,13,6,1,10,8,19,12,22,4,17}, 0, 25, 100, false)]
+        [TestCase(new long[] {15,21,2,20,3,7,25,5,24,23,14,18,16,11,9,13,6,1,10,8,19,12,22,4,17}, 0, 25, 50,  false)]
+        [TestCase(new long[] {20,21,2,15,3,7,25,5,24,23,14,18,16,11,9,13,6,1,10,8,19,12,22,4,17,45}, 1, 25, 26, true)]
+        [TestCase(new long[] {20,21,2,15,3,7,25,5,24,23,14,18,16,11,9,13,6,1,10,8,19,12,22,4,17,45}, 1, 25, 65, false)]
+        [TestCase(new long[] {20,21,2,15,3,7,25,5,24,23,14,18,16,11,9,13,6,1,10,8,19,12,22,4,17,45}, 1, 25, 64, true)]
+        [TestCase(new long[] {20,21,2,15,3,7,25,5,24,23,14,18,16,11,9,13,6,1,10,8,19,12,22,4,17,45}, 1, 25, 66, true)]
         public void HaveTwoNumbersWithSum(long[] numbers, int offset, int len, long sum, bool result)
         {
             Assert.That(Solver.HaveTwoNumbersWithSum(numbers, offset, len, sum), Is.EqualTo(result));
