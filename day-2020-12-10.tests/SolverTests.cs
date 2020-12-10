338b5293a06b02ab51dd8afcd3f1a68cc4c4a671
tarpaha <ppopchikovsky@gmail.com>
2020-12-10 13:24:15 +0300

    initial structure

---
+++ day-2020-12-10.tests/SolverTests.cs
@@ -0,0 +1,8 @@
+using NUnit.Framework;
+
+namespace day_2020_12_10.tests
+{
+    public class SolverTests
+    {
+    }
+}
---
+++ day-2020-12-10/Solver.cs
@@ -0,0 +1,6 @@
+﻿namespace day_2020_12_10
+{
+    public class Solver
+    {
+    }
+}
