3868a93e89588830a8fc34c61906d90a97450b35
tarpaha <ppopchikovsky@gmail.com>
2020-12-11 17:54:43 +0300

    model2 with tests

---
+++ day-2020-12-11.tests/Model2Tests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+
+namespace day_2020_12_11.tests
+{
+    public class Model2Tests
+    {
+        [TestCase(@".......#.
+...#.....
+.#.......
+.........
+..#L....#
+....#....
+.........
+#........
+...#.....",
+            3, 4, 8)]
+        
+        [TestCase(@"
+.............
+.L.L.#.#.#.#.
+.............",
+            1, 1, 1)]
+
+        [TestCase(@"
+.............
+.L.L.........
+.............",
+            1, 1, 1)]
+        
+        [TestCase(@"
+.##.##.
+#.#.#.#
+##...##
+...L...
+##...##
+#.#.#.#
+.##.##.",
+            3, 3, 0)]
+        
+        public void GetVisibleOccupiedSeatsCount_Works_Correctly(string data, int x, int y, int result)
+        {
+            Assert.That(Model2.GetVisibleSeatsCount(Parser.Parse(data), x, y), Is.EqualTo(result));
+        }
+
+        [TestCase(@"L..", 1, 0, -1, 0, 1)]
+        [TestCase(@"L..", 1, 0, +1, 0, 0)]
+        public void IsSeatVisible_Works_Correctly(string data, int x, int y, int dx, int dy, int result)
+        {
+            Assert.That(Model2.IsSeatVisible(Parser.Parse(data), x, y, dx, dy), Is.EqualTo(result));
+        }
+    }
+}
---
+++ day-2020-12-11/Model2.cs
@@ -0,0 +1,43 @@
+﻿namespace day_2020_12_11
+{
+    public static class Model2
+    {
+        public static (Cell[,], bool) Step(Cell[,] input)
+        {
+            return (input, false);
+        }
+
+        public static int GetVisibleSeatsCount(Cell[,] cells, int x, int y)
+        {
+            return
+                IsSeatVisible(cells, x, y,  1,  0) +
+                IsSeatVisible(cells, x, y,  1,  1) +
+                IsSeatVisible(cells, x, y,  0,  1) +
+                IsSeatVisible(cells, x, y, -1,  1) +
+                IsSeatVisible(cells, x, y, -1,  0) +
+                IsSeatVisible(cells, x, y, -1, -1) +
+                IsSeatVisible(cells, x, y,  0, -1) +
+                IsSeatVisible(cells, x, y,  1, -1);
+        }
+
+        public static int IsSeatVisible(Cell[,] cells, int x, int y, int dx, int dy)
+        {
+            var width = cells.GetLength(0);
+            var height = cells.GetLength(1);
+
+            while (true)
+            {
+                x += dx;
+                if (x < 0 || x >= width)
+                    return 0;
+                
+                y += dy;
+                if (y < 0 || y >= height)
+                    return 0;
+
+                if (cells[x, y] != Cell.Floor)
+                    return 1;
+            }
+        }
+    }
+}
