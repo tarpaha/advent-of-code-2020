7c07986b1249f1a4488d6c7473ccb80cd8735d56
tarpaha <ppopchikovsky@gmail.com>
2020-12-11 14:06:40 +0300

    model with tests

---
+++ day-2020-12-11.tests/ModelTests.cs
@@ -0,0 +1,144 @@
+﻿using NUnit.Framework;
+
+namespace day_2020_12_11.tests
+{
+    public class ModelTests
+    {
+        [TestCase(@"
+L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL",
+            @"
+#.##.##.##
+#######.##
+#.#.#..#..
+####.##.##
+#.##.##.##
+#.#####.##
+..#.#.....
+##########
+#.######.#
+#.#####.##")]
+        [TestCase(@"
+#.##.##.##
+#######.##
+#.#.#..#..
+####.##.##
+#.##.##.##
+#.#####.##
+..#.#.....
+##########
+#.######.#
+#.#####.##",
+            @"
+#.LL.L#.##
+#LLLLLL.L#
+L.L.L..L..
+#LLL.LL.L#
+#.LL.LL.LL
+#.LLLL#.##
+..L.L.....
+#LLLLLLLL#
+#.LLLLLL.L
+#.#LLLL.##")]
+        [TestCase(@"
+#.LL.L#.##
+#LLLLLL.L#
+L.L.L..L..
+#LLL.LL.L#
+#.LL.LL.LL
+#.LLLL#.##
+..L.L.....
+#LLLLLLLL#
+#.LLLLLL.L
+#.#LLLL.##",
+            @"
+#.##.L#.##
+#L###LL.L#
+L.#.#..#..
+#L##.##.L#
+#.##.LL.LL
+#.###L#.##
+..#.#.....
+#L######L#
+#.LL###L.L
+#.#L###.##")]
+        [TestCase(@"
+#.##.L#.##
+#L###LL.L#
+L.#.#..#..
+#L##.##.L#
+#.##.LL.LL
+#.###L#.##
+..#.#.....
+#L######L#
+#.LL###L.L
+#.#L###.##",
+            @"
+#.#L.L#.##
+#LLL#LL.L#
+L.L.L..#..
+#LLL.##.L#
+#.LL.LL.LL
+#.LL#L#.##
+..L.L.....
+#L#LLLL#L#
+#.LLLLLL.L
+#.#L#L#.##")]
+        [TestCase(@"
+#.#L.L#.##
+#LLL#LL.L#
+L.L.L..#..
+#LLL.##.L#
+#.LL.LL.LL
+#.LL#L#.##
+..L.L.....
+#L#LLLL#L#
+#.LLLLLL.L
+#.#L#L#.##",
+            @"
+#.#L.L#.##
+#LLL#LL.L#
+L.#.L..#..
+#L##.##.L#
+#.#L.LL.LL
+#.#L#L#.##
+..L.L.....
+#L#L##L#L#
+#.LLLLLL.L
+#.#L#L#.##")]
+        public void Step_Works_Correctly(string dataBefore, string dataAfter)
+        {
+            var cellsBefore = Parser.Parse(dataBefore);
+            var cellsAfter = Parser.Parse(dataAfter);
+            Assert.That(Model.Step(cellsBefore), Is.EquivalentTo(cellsAfter));
+        }
+
+        [Test]
+        public void GetAdjacentOccupiedSeatsCount_Works_Correctly()
+        {
+            const string data = @"
+L#.
+L##
+###
+";
+            var cells = Parser.Parse(data);
+            Assert.That(Model.GetAdjacentOccupiedSeatsCount(cells, 0, 0), Is.EqualTo(2));
+            Assert.That(Model.GetAdjacentOccupiedSeatsCount(cells, 1, 0), Is.EqualTo(2));
+            Assert.That(Model.GetAdjacentOccupiedSeatsCount(cells, 2, 0), Is.EqualTo(3));
+            Assert.That(Model.GetAdjacentOccupiedSeatsCount(cells, 0, 1), Is.EqualTo(4));
+            Assert.That(Model.GetAdjacentOccupiedSeatsCount(cells, 1, 1), Is.EqualTo(5));
+            Assert.That(Model.GetAdjacentOccupiedSeatsCount(cells, 2, 1), Is.EqualTo(4));
+            Assert.That(Model.GetAdjacentOccupiedSeatsCount(cells, 0, 2), Is.EqualTo(2));
+            Assert.That(Model.GetAdjacentOccupiedSeatsCount(cells, 1, 2), Is.EqualTo(4));
+            Assert.That(Model.GetAdjacentOccupiedSeatsCount(cells, 2, 2), Is.EqualTo(3));
+        }
+    }
+}
---
+++ day-2020-12-11/Model.cs
@@ -0,0 +1,63 @@
+﻿namespace day_2020_12_11
+{
+    public static class Model
+    {
+        public static Cell[,] Step(Cell[,] input)
+        {
+            var width = input.GetLength(0);
+            var height = input.GetLength(1);
+            
+            var output = new Cell[width, height];
+
+            for (var y = 0; y < height; y++)
+            {
+                for (var x = 0; x < width; x++)
+                {
+                    switch (input[x, y])
+                    {
+                        case Cell.EmptySeat:
+                            output[x, y] = GetAdjacentOccupiedSeatsCount(input, x, y) == 0
+                                ? Cell.OccupiedSeat
+                                : Cell.EmptySeat;
+                            break;
+                        case Cell.OccupiedSeat:
+                            output[x, y] = GetAdjacentOccupiedSeatsCount(input, x, y) >= 4
+                                ? Cell.EmptySeat
+                                : Cell.OccupiedSeat;
+                            break;
+                    }
+                }
+            }
+
+            return output;
+        }
+
+        public static int GetAdjacentOccupiedSeatsCount(Cell[,] cells, int x, int y)
+        {
+            var width = cells.GetLength(0);
+            var height = cells.GetLength(1);
+
+            var count = 0;
+            
+            for (var yi = y - 1; yi <= y + 1; yi++)
+            {
+                if(yi < 0 || yi >= height)
+                    continue;
+                
+                for (var xi = x - 1; xi <= x + 1; xi++)
+                {
+                    if(xi < 0 || xi >= width)
+                        continue;
+                    
+                    if(xi == x && yi == y)
+                        continue;
+
+                    if (cells[xi, yi] == Cell.OccupiedSeat)
+                        count += 1;
+                }
+            }
+
+            return count;
+        }
+    }
+}
