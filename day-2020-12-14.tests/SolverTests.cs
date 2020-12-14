cd37122532981e026a4ddf9a7644e2310d4d5b03
tarpaha <ppopchikovsky@gmail.com>
2020-12-14 19:30:47 +0300

    apply mask with tests

--- day-2020-12-14.tests/SolverTests.cs
+++ day-2020-12-14.tests/SolverTests.cs
@@ -4,5 +4,12 @@ namespace day_2020_12_14.tests
 {
     public class SolverTests
     {
+        [TestCase(11, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", 73)]
+        [TestCase(101, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", 101)]
+        [TestCase(0, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", 64)]
+        public void ApplyMask_Works_Correctly(long number, string mask, long result)
+        {
+            Assert.That(Solver.ApplyMask(number, mask), Is.EqualTo(result));
+        }
     }
 }
--- day-2020-12-14/Solver.cs
+++ day-2020-12-14/Solver.cs
@@ -2,5 +2,22 @@
 {
     public static class Solver
     {
+        public static long ApplyMask(long number, string mask)
+        {
+            long bit = 1;
+            for (var i = mask.Length - 1; i >= 0; i--, bit <<= 1)
+            {
+                switch (mask[i])
+                {
+                    case '0':
+                        number &= ~bit;
+                        break;
+                    case '1':
+                        number |= bit;
+                        break;
+                }
+            }
+            return number;
+        }
     }
 }
