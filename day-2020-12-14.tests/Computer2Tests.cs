e7421cbc909a2c4a307e768100034003271eb4d1
tarpaha <ppopchikovsky@gmail.com>
2020-12-14 21:54:56 +0300

    expand mask with tests

---
+++ day-2020-12-14.tests/Computer2Tests.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace day_2020_12_14.tests
+{
+    public class Computer2Tests
+    {
+        [TestCase("XX", new long[] { 0, 1, 2, 3 })]
+        [TestCase("00000000000000000000000000000000X0XX", new long[] { 0, 1, 2, 3, 8, 9, 10, 11 })]
+        public void ExpandMask_Works_Correctly(string mask, long[] result)
+        {
+            var masks = Computer2.ExpandMask(mask);
+            Assert.That(masks.OrderBy(m => m), Is.EquivalentTo(result.OrderBy(m => m)));
+        }
+    }
+}
---
+++ day-2020-12-14/Computer2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace day_2020_12_14
+{
+    public class Computer2
+    {
+        public static IEnumerable<long> ExpandMask(string maskTemplate)
+        {
+            var masks = new List<string> {maskTemplate};
+            for (var i = 0; i < masks.Count; i++)
+            { 
+                var xPos = masks[i].IndexOf('X');
+                if (xPos < 0)
+                    continue;
+
+                var mask = new StringBuilder(masks[i]);
+
+                mask[xPos] = '0';
+                masks[i] = mask.ToString();
+                
+                mask[xPos] = '1';
+                masks.Add(mask.ToString());
+
+                i -= 1;
+            }
+            return masks.Select(m => Convert.ToInt64(m, 2)).ToList();
+        }
+    }
+}
