d4d48c9eb073c3342b88f1df435833b415dc2956
tarpaha <ppopchikovsky@gmail.com>
2021-01-18 20:58:31 +0300

    Grid4D (copy of Grid3D with one more dimension)

---
+++ day-2020-12-17/Grid4D.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace day_2020_12_17
+{
+    public class Grid4D
+    {
+        public int ActiveCubesCount => _activeCubes.Count;
+        
+        public void SetCubeActive(int x, int y, int z, int w)
+        {
+            _activeCubes.Add((x, y, z, w));
+            UpdateSize(x, y, z, w);
+        }
+
+        public bool IsCubeActive(int x, int y, int z, int w)
+        {
+            return _activeCubes.Contains((x, y, z, w));
+        }
+
+        public ((int, int), (int, int), (int, int), (int, int)) GetDimensions()
+        {
+            return (_x, _y, _z, _w);
+        }
+        
+        private void UpdateSize(int x, int y, int z, int w)
+        {
+            if (x < _x.min)
+                _x.min = x;
+            if (x > _x.max)
+                _x.max = x;
+            
+            if (y < _y.min)
+                _y.min = y;
+            if (y > _y.max)
+                _y.max = y;
+
+            if (z < _z.min)
+                _z.min = z;
+            if (z > _z.max)
+                _z.max = z;
+            
+            if (w < _w.min)
+                _w.min = w;
+            if (w > _w.max)
+                _w.max = w;
+        }
+        
+        private readonly HashSet<(int, int, int, int)> _activeCubes = new();
+
+        private (int min, int max) _x = (int.MaxValue, int.MinValue);
+        private (int min, int max) _y = (int.MaxValue, int.MinValue);
+        private (int min, int max) _z = (int.MaxValue, int.MinValue);
+        private (int min, int max) _w = (int.MaxValue, int.MinValue);
+    }
+}
