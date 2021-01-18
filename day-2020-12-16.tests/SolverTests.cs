cc1194b4131e5e181807a782321a7121e75919ae
tarpaha <ppopchikovsky@gmail.com>
2021-01-18 12:19:29 +0300

    one more test for removing invalid tickets

--- day-2020-12-16.tests/SolverTests.cs
+++ day-2020-12-16.tests/SolverTests.cs
@@ -50,6 +50,18 @@ nearby tickets:
 40,4,50
 55,2,20
 38,6,12", 1)]        
+        [TestCase(@"
+class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9", 3)]        
         public void RemoveInvalidTickets_Works_Correctly(string data, int validTicketsCount)
         {
             Assert.That(Solver.RemoveInvalidTickets(Parser.Parse(data)).NearbyTickets.Count(), Is.EqualTo(validTicketsCount));
