7d593aa9aa29842dbd5e6c91aaad487e908f988f
tarpaha <ppopchikovsky@gmail.com>
2021-01-18 03:12:36 +0300

    function to remove invalid tickets with test

--- day-2020-12-16.tests/SolverTests.cs
+++ day-2020-12-16.tests/SolverTests.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using NUnit.Framework;
 
 namespace day_2020_12_16.tests
@@ -34,6 +35,24 @@ nearby tickets:
         public void NumberIsValid_Works_Correctly(string ruleStr, int number, bool result)
         {
             Assert.That(Solver.NumberIsValid(number, Parser.ParseRule(ruleStr)), Is.EqualTo(result));
+        }
+        
+        [TestCase(@"
+class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12", 1)]        
+        public void RemoveInvalidTickets_Works_Correctly(string data, int validTicketsCount)
+        {
+            Assert.That(Solver.RemoveInvalidTickets(Parser.Parse(data)).NearbyTickets.Count(), Is.EqualTo(validTicketsCount));
         }
     }
 }
--- day-2020-12-16/Solver.cs
+++ day-2020-12-16/Solver.cs
@@ -9,10 +9,24 @@ namespace day_2020_12_16
             var rules = problem.Rules.ToList();
             return problem.NearbyTickets
                 .SelectMany(ticket => ticket.Numbers)
-                .Where(number => !rules.Any(rule => NumberIsValid(number, rule)))
+                .Where(number => rules.All(rule => !NumberIsValid(number, rule)))
                 .Sum();
         }
 
+        public static int Part2(Problem problem)
+        {
+            problem = RemoveInvalidTickets(problem);
+            return 0;
+        }
+
+        public static Problem RemoveInvalidTickets(Problem problem)
+        {
+            var rules = problem.Rules.ToList();
+            var validTickets = problem.NearbyTickets
+                .Where(ticket => ticket.Numbers.All(number => rules.Any(rule => NumberIsValid(number, rule)))).ToList();
+            return new Problem(rules, problem.YourTicket, validTickets);
+        }
+        
         public static bool NumberIsValid(int number, Rule rule)
         {
             return ((rule.Range1.min <= number) && (number <= rule.Range1.max)) ||
