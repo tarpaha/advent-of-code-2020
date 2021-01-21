9c0c59e1e6b743a88ef24669f46a8ac23e24b0bc
tarpaha <ppopchikovsky@gmail.com>
2021-01-21 18:27:41 +0300

    Solver.IsMessageValid with tests

--- day-2020-12-19.tests/SolverTests.cs
+++ day-2020-12-19.tests/SolverTests.cs
@@ -4,5 +4,39 @@ namespace day_2020_12_19.tests
 {
     public class SolverTests
     {
+        [TestCase("aab", true)]
+        [TestCase("aba", true)]
+        [TestCase("bba", false)]
+        [TestCase("abb", false)]
+        [TestCase("abaa", false)]
+        public void IsMessageValid_Works_Correctly01(string message, bool result)
+        {
+            const string data = @"
+0: 1 2
+1: ""a""
+2: 1 3 | 3 1
+3: ""b"""; 
+            var rules = RulesParser.Parse(data);
+            Assert.That(Solver.IsMessageValid(rules[0], message), Is.EqualTo(result));
+        }
+        
+        [TestCase("ababbb", true)]
+        [TestCase("abbbab", true)]
+        [TestCase("bababa", false)]
+        [TestCase("aaabbb", false)]
+        [TestCase("aaaabbb", false)]
+        public void IsMessageValid_Works_Correctly02(string message, bool result)
+        {
+            const string data = @"
+0: 4 1 5
+1: 2 3 | 3 2
+2: 4 4 | 5 5
+3: 4 5 | 5 4
+4: ""a""
+5: ""b"""; 
+            var rules = RulesParser.Parse(data);
+            Assert.That(Solver.IsMessageValid(rules[0], message), Is.EqualTo(result));
+        }
+        
     }
 }
--- day-2020-12-19/Solver.cs
+++ day-2020-12-19/Solver.cs
@@ -1,6 +1,14 @@
-﻿namespace day_2020_12_19
+﻿using day_2020_12_19.Rules;
+
+namespace day_2020_12_19
 {
     public static class Solver
     {
+        public static bool IsMessageValid(IRule rule, string message)
+        {
+            var pos = 0;
+            var result = rule.Match(message, ref pos);
+            return result && pos == message.Length;
+        }
     }
 }
