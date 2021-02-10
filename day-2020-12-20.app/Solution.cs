9d96c9c0a95a5f151ab556ee44d1a44543c77797
tarpaha <ppopchikovsky@gmail.com>
2021-02-11 00:02:24 +0300

    day-2020-12-20 part1

--- day-2020-12-20.app/Solution.cs
+++ day-2020-12-20.app/Solution.cs
@@ -1,20 +1,29 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 using utils;
 
 namespace day_2020_12_20.app
 {
     public class Solution : ISolution
     {
+        private readonly IEnumerable<Tile> _tiles;
+
         public static void Main()
         {
             var solution = new Solution();
             Console.WriteLine($"Part1: {solution.SolvePart1()}");
             Console.WriteLine($"Part2: {solution.SolvePart2()}");
         }
-        
+
+        public Solution()
+        {
+            _tiles = Parser.ParseTiles(Input.GetData()).ToList();
+        }
+
         public object SolvePart1()
         {
-            return null;
+            return Solver.Part1(_tiles);
         }
 
         public object SolvePart2()
--- day-2020-12-20/Solver.cs
+++ day-2020-12-20/Solver.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
+using System.Collections.Generic;
 
 namespace day_2020_12_20
 {
@@ -6,12 +7,35 @@ namespace day_2020_12_20
     {
         public static long Part1(IEnumerable<Tile> tiles)
         {
-            var sides = new List<(int hash, int id)>();
+            var pairs = new List<(int hash, int id)>();
             foreach (var tile in tiles)
             {
+                pairs.AddRange(GetHashes(tile).Select(hash => (hash, tile.Id)));
             }
-            
-            return 0;
+
+            return pairs
+                .GroupBy(pair => pair.hash) // group by hashes
+                .Where(group => group.Count() == 1) // only unique hashes 
+                .Select(group => (id: group.First().id, hash: group.Key))
+                .GroupBy(tuple => tuple.id) // group by ids
+                .Where(group => group.Count() == 4) // only ids with 4 unique hashes
+                .Select(group => group.Key)
+                .Aggregate(1L, (x, y) => x * y);
+        }
+
+        public static IEnumerable<int> GetHashes(Tile tile)
+        {
+            return new[]
+            {
+                GetHash(tile, 0, 0, +1,  0),
+                GetHash(tile, 9, 0, -1,  0),
+                GetHash(tile, 0, 0,  0, +1),
+                GetHash(tile, 0, 9,  0, -1),
+                GetHash(tile, 0, 9, +1,  0),
+                GetHash(tile, 9, 9, -1,  0),
+                GetHash(tile, 9, 0,  0, +1),
+                GetHash(tile, 9, 9,  0, -1)
+            };
         }
 
         public static int GetHash(Tile tile, int x, int y, int dx, int dy)
