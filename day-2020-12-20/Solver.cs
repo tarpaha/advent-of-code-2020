8bb774ac44e03d9f3e9f6c511694568747e7e417
tarpaha <ppopchikovsky@gmail.com>
2021-02-10 23:36:05 +0300

    get tile hash with tests

--- day-2020-12-20/Solver.cs
+++ day-2020-12-20/Solver.cs
@@ -1,6 +1,32 @@
-﻿namespace day_2020_12_20
+﻿using System.Collections.Generic;
+
+namespace day_2020_12_20
 {
-    public class Solver
+    public static class Solver
     {
+        public static long Part1(IEnumerable<Tile> tiles)
+        {
+            var sides = new List<(int hash, int id)>();
+            foreach (var tile in tiles)
+            {
+            }
+            
+            return 0;
+        }
+
+        public static int GetHash(Tile tile, int x, int y, int dx, int dy)
+        {
+            var hash = 0;
+            var n = 1 << (tile.Size - 1);
+            for (var i = 0; i < tile.Size; i++)
+            {
+                if (tile.GetCell(x, y))
+                    hash += n;
+                x += dx;
+                y += dy;
+                n >>= 1;
+            }
+            return hash;
+        }
     }
 }
