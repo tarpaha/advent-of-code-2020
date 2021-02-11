bb563917ea64a431d56d2950dcee123402c66de6
tarpaha <ppopchikovsky@gmail.com>
2021-02-11 19:57:31 +0300

    rotate tile clockwise with tests

---
+++ day-2020-12-20.tests/TileOperationsTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+
+namespace day_2020_12_20.tests
+{
+    public class TileOperationsTests
+    {
+        [TestCase(@"
+Tile 2311:
+..##.#..#.
+##..#.....
+#...##..#.
+####.#...#
+##.##.###.
+##...#.###
+.#.#.#..##
+..#....#..
+###...#.#.
+..###..###")]
+        public void RotateClockwise_Works_Correctly(string data)
+        {
+            var tile = Parser.ParseTile(data);
+            var rotatedTile = TileOperations.RotateClockwise(tile);
+            
+            Assert.That(tile.Top, Is.EqualTo(rotatedTile.Right));
+            Assert.That(tile.Right, Is.EqualTo(rotatedTile.Bottom));
+            Assert.That(tile.Bottom, Is.EqualTo(rotatedTile.Left));
+            Assert.That(tile.Left, Is.EqualTo(rotatedTile.Top));
+        }
+
+        [TestCase(@"
+Tile 2311:
+..##.#..#.
+##..#.....
+#...##..#.
+####.#...#
+##.##.###.
+##...#.###
+.#.#.#..##
+..#....#..
+###...#.#.
+..###..###")]
+        public void RotateClockwise_4Times_ReturnsTheSame(string data)
+        {
+            var source = Parser.ParseTile(data);
+            var rotated = TileOperations.RotateClockwise(source);
+            rotated = TileOperations.RotateClockwise(rotated);
+            rotated = TileOperations.RotateClockwise(rotated);
+            rotated = TileOperations.RotateClockwise(rotated);
+            
+            Assert.That(source.Top, Is.EqualTo(rotated.Top));
+            Assert.That(source.Bottom, Is.EqualTo(rotated.Bottom));
+            Assert.That(source.Left, Is.EqualTo(rotated.Left));
+            Assert.That(source.Right, Is.EqualTo(rotated.Right));
+        }
+    }
+}
---
+++ day-2020-12-20/TileOperations.cs
@@ -0,0 +1,18 @@
+﻿namespace day_2020_12_20
+{
+    public static class TileOperations
+    {
+        public static Tile RotateClockwise(Tile tile)
+        {
+            var rotatedCells = new bool[tile.Size, tile.Size];
+            for (var y = 0; y < tile.Size; y++)
+            {
+                for (var x = 0; x < tile.Size; x++)
+                {
+                    rotatedCells[tile.Size - 1 - y, x] = tile.GetCell(x, y);
+                }
+            }
+            return new Tile(tile.Id, rotatedCells);
+        }
+    }
+}
