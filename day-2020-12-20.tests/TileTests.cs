f8c2abc45324265befbbfa22d626667652817971
tarpaha <ppopchikovsky@gmail.com>
2021-02-11 19:32:04 +0300

    tile border hash calculation with tests

---
+++ day-2020-12-20.tests/TileTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace day_2020_12_20.tests
+{
+    public class TileTests
+    {
+        [TestCase(@"
+Tile 2311:
+..##.#..#.
+##..#.....
+#...##..#.
+####.#...#
+##.##.###.
+##...#.###
+.#.#.#..##
+..#....#..
+###...#.#.
+..###..###",
+            0b0111110010,
+            0b0001011001,
+            0b0011010010,
+            0b0011100111)]
+        public void TileBorderHash_Works_Correctly(string data, int left, int right, int top, int bottom)
+        {
+            var tile = Parser.ParseTile(data);
+            Assert.That(tile.Left, Is.EqualTo(left));
+            Assert.That(tile.Right, Is.EqualTo(right));
+            Assert.That(tile.Top, Is.EqualTo(top));
+            Assert.That(tile.Bottom, Is.EqualTo(bottom));
+        }
+    }
+}
--- day-2020-12-20/Tile.cs
+++ day-2020-12-20/Tile.cs
@@ -4,6 +4,12 @@
     {
         public int Id { get; }
         public int Size => _cells.GetLength(0);
+
+        public int Left { get; }
+        public int Right { get; }
+        public int Top { get; }
+        public int Bottom { get; }
+        
         public bool GetCell(int x, int y) => _cells[x, y];
 
         private readonly bool[,] _cells;
@@ -12,6 +18,26 @@
         {
             Id = id;
             _cells = cells;
+
+            Top    = CalculateBorderHash(0, 0, 1, 0);
+            Bottom = CalculateBorderHash(0, Size - 1, 1, 0);
+            Left   = CalculateBorderHash(0, 0, 0, 1);
+            Right  = CalculateBorderHash(Size - 1, 0, 0, 1);
+        }
+
+        private int CalculateBorderHash(int x, int y, int dx, int dy)
+        {
+            var hash = 0;
+            var n = 1 << (Size - 1);
+            for (var i = 0; i < Size; i++)
+            {
+                if (GetCell(x, y))
+                    hash += n;
+                x += dx;
+                y += dy;
+                n >>= 1;
+            }
+            return hash;
         }
     }
 }
