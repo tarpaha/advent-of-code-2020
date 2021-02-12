cae3b01717c69011fe72ef80c3bdf4ff179c75ff
tarpaha <ppopchikovsky@gmail.com>
2021-02-12 19:17:02 +0300

    do not calculate border hash for special tile representing final image

--- day-2020-12-20/Tile.cs
+++ day-2020-12-20/Tile.cs
@@ -20,10 +20,13 @@
             _cells = cells;
             Size = _cells.GetLength(0);
 
-            Top    = BorderHash.Calculate(this, 0, 0, 1, 0);
-            Bottom = BorderHash.Calculate(this, 0, Size - 1, 1, 0);
-            Left   = BorderHash.Calculate(this, 0, 0, 0, 1);
-            Right  = BorderHash.Calculate(this, Size - 1, 0, 0, 1);
+            if (Id > 0)
+            {
+                Top    = BorderHash.Calculate(this, 0, 0, 1, 0);
+                Bottom = BorderHash.Calculate(this, 0, Size - 1, 1, 0);
+                Left   = BorderHash.Calculate(this, 0, 0, 0, 1);
+                Right  = BorderHash.Calculate(this, Size - 1, 0, 0, 1);
+            }
         }
 
         public override string ToString()
