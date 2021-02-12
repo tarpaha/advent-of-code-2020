7f3f997495261df2dd2f29d4aacc1111e45518e0
tarpaha <ppopchikovsky@gmail.com>
2021-02-12 19:16:18 +0300

    tile to string conversion was extracted to TileOperations

--- day-2020-12-20.tests/ParserTests.cs
+++ day-2020-12-20.tests/ParserTests.cs
@@ -21,7 +21,7 @@ Tile 2311:
         public void ParseTile_Works_Correctly(string data)
         {
             var tile = Parser.ParseTile(data);
-            Assert.That(Environment.NewLine + TileToString(tile), Is.EqualTo(data));
+            Assert.That(Environment.NewLine + TileOperations.ToString(tile), Is.EqualTo(data));
         }
 
         [TestCase(@"
@@ -63,21 +63,8 @@ Tile 1171:
         public void ParseTiles_Works_Correctly(string data)
         {
             var tiles = Parser.ParseTiles(data);
-            var result = Environment.NewLine + string.Join($"{Environment.NewLine}{Environment.NewLine}", tiles.Select(TileToString));
+            var result = Environment.NewLine + string.Join($"{Environment.NewLine}{Environment.NewLine}", tiles.Select(TileOperations.ToString));
             Assert.That(result, Is.EqualTo(data));
-        }
-
-        private static string TileToString(Tile tile)
-        {
-            var result = $"Tile {tile.Id}:{Environment.NewLine}";
-            for (var y = 0; y < tile.Size; y++)
-            {
-                for (var x = 0; x < tile.Size; x++)
-                    result += tile.GetCell(x, y) ? '#' : '.';
-                if(y < tile.Size - 1)
-                    result += Environment.NewLine;
-            }
-            return result;
         }
     }
 }
--- day-2020-12-20/TileOperations.cs
+++ day-2020-12-20/TileOperations.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace day_2020_12_20
 {
@@ -56,6 +57,21 @@ namespace day_2020_12_20
                 flipped = RotateClockwise(flipped);
             }
             return variations;
+        }
+
+        public static string ToString(Tile tile)
+        {
+            var result = tile.Id != 0
+                ? $"Tile {tile.Id}:{Environment.NewLine}"
+                : "";
+            for (var y = 0; y < tile.Size; y++)
+            {
+                for (var x = 0; x < tile.Size; x++)
+                    result += tile.GetCell(x, y) ? '#' : '.';
+                if(y < tile.Size - 1)
+                    result += Environment.NewLine;
+            }
+            return result;
         }
     }
 }
