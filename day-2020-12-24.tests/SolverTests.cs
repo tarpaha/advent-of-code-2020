9c970036ebe041988e5abe5cc03622eb271f3125
tarpaha <ppopchikovsky@gmail.com>
2021-03-24 17:50:09 +0300

    move function with tests

--- day-2020-12-24.tests/SolverTests.cs
+++ day-2020-12-24.tests/SolverTests.cs
@@ -15,5 +15,13 @@ namespace day_2020_12_24.tests
         {
             Assert.That(Solver.Part2(), Is.Null);
         }
+
+        [TestCase(0, 0, "nwwswee", 0, 0)]
+        [TestCase(0, 0, "nwswseenenwsw", 0, 0)]
+        public void Move_Works_Correctly(int x1, int y1, string str, int x2, int y2)
+        {
+            var result = Solver.Move((x1, y1), Parser.Parse(str));
+            Assert.That(result, Is.EqualTo((x2, y2)));
+        }
     }
 }
--- day-2020-12-24/Solver.cs
+++ day-2020-12-24/Solver.cs
@@ -1,4 +1,6 @@
-﻿namespace day_2020_12_24
+﻿using System.Collections.Generic;
+
+namespace day_2020_12_24
 {
     public static class Solver
     {
@@ -11,5 +13,38 @@
         {
             return null;
         }
+
+        public static (int x, int y) Move((int x, int y) pos, IEnumerable<Direction> directions)
+        {
+            foreach (var direction in directions)
+            {
+                var (x, y) = DirectionShifts[direction];
+
+                switch (direction)
+                {
+                    case Direction.SW:
+                    case Direction.NW:
+                    case Direction.SE:
+                    case Direction.NE:
+                        if ((pos.y & 1) == 1)
+                            pos.x -= 1;
+                        break;
+                }
+                
+                pos.x += x;
+                pos.y += y;
+            }
+            return pos;
+        }
+        
+        private static readonly Dictionary<Direction, (int x, int y)>  DirectionShifts = new()
+        {
+            { Direction.E,  (+1,  0) },
+            { Direction.W,  (-1,  0) },
+            { Direction.SE, (+1, +1) },
+            { Direction.SW, ( 0, +1) },
+            { Direction.NE, (+1, -1) },
+            { Direction.NW, ( 0, -1) }
+        };
     }
 }
