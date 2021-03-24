4be578d592b0d7349b9ecc30dca932b1a37fb43b
tarpaha <ppopchikovsky@gmail.com>
2021-03-24 17:00:16 +0300

    initial structure

---
+++ day-2020-12-24/Solver.cs
@@ -0,0 +1,15 @@
+﻿namespace day_2020_12_24
+{
+    public static class Solver
+    {
+        public static object Part1()
+        {
+            return null;
+        }
+
+        public static object Part2()
+        {
+            return null;
+        }
+    }
+}
